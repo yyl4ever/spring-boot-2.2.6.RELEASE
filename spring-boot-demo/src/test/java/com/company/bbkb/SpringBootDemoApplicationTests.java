134577fdee8a1128fc78d59257d4adecf6f35b55
yyl4ever <yyl4ever@foxmail.com>
2020-05-04 21:37:27 +0800

    spring-boot 源码学习 - 环境搭建

---
+++ spring-boot-demo/src/test/java/com/company/bbkb/SpringBootDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.company.bbkb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println(1);
+	}
+
+}
